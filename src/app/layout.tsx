550fd66f78a430c5598aa85e8add348c97c951a1
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-09-24 01:12:30 +0300

    PWA fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ export default function RootLayout({
     return (
         <html lang="ru">
         <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="manifest" href="../../public/manifest.json" />
+          <link rel="apple-touch-icon" href="../../public/logo192.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
         <body className='flex items-center justify-center'>
