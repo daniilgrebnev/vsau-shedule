97995ccb4dee681575ee4efceb8c93fb6567de9e
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-09-24 12:43:07 +0300

    Search 6

--- src/features/Search.tsx
+++ src/features/Search.tsx
@@ -4,7 +4,7 @@ const Search = () => {
     return (
         <div className=' flex items-center justify-center w-full'>
             <div className="w-full">
-                <div className="mx-auto border-b  mt-16  text-xl w-full">
+                <div className="mx-auto border-b mt-16  text-xl w-full">
                     <input type='text'
                            placeholder='Поиск'
                            className='focus-within:outline-0 uppercase w-full py-2 px-3 focus:border-0 focus:outline-0 focus-within:border-0 bg-opacity-10 bg-black text-inherit'/>
