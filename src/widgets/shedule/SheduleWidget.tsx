bf64e73e4648be941bf594bddae92a0b76eb75ba
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-09-30 20:24:14 +0300

    version 0.6.3 Calendar

--- src/widgets/shedule/SheduleWidget.tsx
+++ src/widgets/shedule/SheduleWidget.tsx
@@ -17,19 +17,19 @@ const SheduleWidget = () => {
 
     const weekData = weekInit
     return (
-        <div className='p-2'>
-            <div className="flex items-end justify-between px-4 font-semibold text">
-                <div className="grid grid-rows-2 justify-around items-center ">
-                    <p className="text-3xl rounded-lg text-center py-2">{groupName}<span
-                        className="lowercase">{subGroup}</span></p>
+        <div className='p-4'>
+            <div className="flex items-end justify-between font-semibold text">
+                <div className="flex flex-col relative h-full justify-between items-center ">
+
                     <div
-                        className="rounded-lg overflow-hidden bg-opacity-90 w-[150px] relative text-xl text-center text-white">
-                        <div className={`w-full bg-green-600 top-1/2 left-0 h-1/2 absolute z-10`}></div>
-                        <div className="py-3 relative z-40">Числитель</div>
-                        <div className="w-full relative z-40 h-[5px] bg-white "></div>
-                        <div className="py-3 relative z-40">Знаменатель</div>
+                        className="rounded-lg border-4 py-3 px-6 bg-opacity-90 relative text-lg text-center text-white">
+                        {weekData.parity}
                     </div>
+                    
+                    <p className="text-5xl rounded-lg  text-center py-7">{groupName}<span
+                        className="lowercase">{subGroup}</span></p>
                 </div>
+
                 <Calendar month={weekData.month} day={weekData.day} dayOfWeek={weekData.dayOfWeek}/>
 
             </div>
