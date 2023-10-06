7281c6abaf91c0b4152767d18f79cc0fe4bb79fc
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-10-06 14:45:10 +0300

    version 0.7.05 vis

--- src/widgets/shedule/SheduleWidget.tsx
+++ src/widgets/shedule/SheduleWidget.tsx
@@ -23,8 +23,8 @@ const SheduleWidget = () => {
                 <div className="bg-black rounded-br-lg px-3 w-1/2 py-2">{weekData.day}</div>
             </div>
 
-            <div className="mt-12 text-center"><span
-                className='text-5xl py-10 font-semibold'>{groupName}{subGroup}</span></div>
+            <div className="my-12 text-center"><span
+                className='text-5xl font-semibold'>{groupName}{subGroup}</span></div>
             <div className='w-full  h-full flex items-center justify-center'>
                 <Week/>
             </div>
