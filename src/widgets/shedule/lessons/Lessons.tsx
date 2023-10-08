2c5769acf5a61c177fb1175e11430360ffe5fdc2
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-10-08 15:43:02 +0300

    version 0.9.3

--- src/widgets/shedule/lessons/Lessons.tsx
+++ src/widgets/shedule/lessons/Lessons.tsx
@@ -34,26 +34,36 @@ const Lessons = ({day, id}: any) => {
     return (
 
         <div className=" flex flex-col w-full   justify-center gap-y-2">
-            {currentShedule?.lessons[id - 1]?.numerator.map(item => (
-                <div
-                    className='p-2 bg-bg-header relative rounded-lg w-[90%] mx-auto'>
-                    <div className=" w-full text-left text-sm ">
-                        <div className="">{item.time_start} - {item.time_end}</div>
+            {currentShedule?.lessons[id - 1]?.numerator.length !== 0
+                ? currentShedule?.lessons[id - 1]?.numerator.map(item => (
+                    <div>
+
+
+                        <div
+                            className='p-2 bg-bg-header relative rounded-lg w-[90%] mx-auto'>
+                            <div className=" w-full text-left text-sm ">
+                                <div className="">{item.time_start} - {item.time_end}</div>
+
+                            </div>
+                            <div className="">
+                                <div className="mt-5 flex items-center w-full justify-start">
+                                    <div className="">{item.name}</div>
+                                    <div className="mx-3 px-2 py-1 bg-bg-main rounded-lg">{item.room}</div>
+                                </div>
+                                <div style={{background: `${types.find(i => i.type === item.type)?.color}`}}
+                                     className={`absolute text-sm top-1 right-1 flex items-center justify-center bg-[] w-14 h-6 rounded-lg`}>
+                                    {types.find(i => i.type === item.type)?.name}
+                                </div>
+                                <div className="mt-2">{item.teacher}</div>
+                            </div>
+                        </div>
+
 
                     </div>
-                    <div className="">
-                        <div className="mt-5 flex items-center w-full justify-start">
-                            <div className="">{item.name}</div>
-                            <div className="mx-3 px-2 py-1 bg-bg-main rounded-lg">{item.room}</div>
-                        </div>
-                        <div style={{background: `${types.find(i => i.type === item.type)?.color}`}}
-                             className={`absolute text-sm top-1 right-1 flex items-center justify-center bg-[] w-14 h-6 rounded-lg`}>
-                            {types.find(i => i.type === item.type)?.name}
-                        </div>
-                        <div className="mt-2">{item.teacher}</div>
-                    </div>
-                </div>
-            ))}
+                )) :
+                <div className='w-full h-full justify-center items-center text-center text-3xl'>Нет пар</div>
+
+            }
         </div>
 
     )
