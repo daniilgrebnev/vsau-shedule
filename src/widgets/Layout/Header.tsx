60d51e86d7fd047b92b14bf2ed18d7d64b321e3d
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-10-11 16:55:20 +0300

    0.10.9

--- src/widgets/Layout/Header.tsx
+++ src/widgets/Layout/Header.tsx
@@ -1,48 +1,59 @@
 'use client'
-import React, {useState} from 'react'
-import LogoIcon from "@/entities/icons/LogoIcon";
-import Search from "@/features/Search";
-import {useAppDispatch, useAppSelector} from "@/hooks";
-import {searchHandler} from "@/store/slices/searchSlice";
-import SettingsIcon from "@/entities/icons/SettingsIcon";
-import Link from "next/link";
-import SearchIcon from "@/entities/icons/SearchIcon";
+import LogoIcon from '@/entities/icons/LogoIcon'
+import SearchIcon from '@/entities/icons/SearchIcon'
+import SettingsIcon from '@/entities/icons/SettingsIcon'
+import Search from '@/features/Search'
+import { useAppDispatch, useAppSelector } from '@/hooks'
+import { searchHandler } from '@/store/slices/searchSlice'
+import Link from 'next/link'
+import { useState } from 'react'
 
 const Header = () => {
-    const open = useAppSelector(store => store.searchReducer.open)
-    const dispatch = useAppDispatch()
-    const [openSearch, setOpenSearch] = useState(open)
-    console.log(openSearch)
-    return (
-        <div
-            className='flex items-end w-full z-50 justify-between p-4 rounded-b-2xl bg-bg-header text-white'>
-            <div className="flex items-end justify-between">
-                <div className="flex items-center"><LogoIcon fill='white' width={40}/></div>
+	const open = useAppSelector(store => store.searchReducer.open)
+	const dispatch = useAppDispatch()
+	const [openSearch, setOpenSearch] = useState(open)
+	console.log(openSearch)
+	const groups = useAppSelector(state => state.sheduleReducer.initialState.list)
+	const groupId = groups[groups.length - 1]?.groupId
+	return (
+		<div className='flex items-end w-full z-50 justify-between p-4 rounded-b-2xl bg-bg-header text-white'>
+			<div className='flex items-end justify-between'>
+				<div className='flex items-center'>
+					<LogoIcon fill='white' width={40} />
+				</div>
 
-                <div className="text-center text-2xl font-semibold ml-3">Расписание</div>
-            </div>
-            <div className="flex items-center justify-between w-[23%]">
-                <div className="flex items-start justify-center relative -top-1"
-                     onClick={() => dispatch(searchHandler())}>
-                    <SearchIcon fill='white' width={30}/>
-                </div>
-                <div className="flex items-start justify-center relative -top-1">
-                    <Link href='settings'>
-                        <SettingsIcon fill='white' width={30}/>
-                    </Link>
-                </div>
-            </div>
-            {
-                open ? <div
-                    className="absolute transition-all flex items-center justify-center top-0 left-0 w-full h-full bg-black bg-opacity-90 ">
-                    <div className="bg-bg-main z-50 border-1 z-40 border-white overflow-hidden w-full h-full ">
-                        <Search/>
-                    </div>
-                </div> : <></>
-            }
+				<div className='text-center text-2xl font-semibold ml-3'>
+					Расписание
+				</div>
+			</div>
+			<div className='flex items-center justify-end gap-3 w-[23%]'>
+				{groupId == '-1' ? (
+					''
+				) : (
+					<div
+						className='flex w-full items-center justify-end relative -top-1'
+						onClick={() => dispatch(searchHandler())}
+					>
+						<SearchIcon fill='white' width={30} />
+					</div>
+				)}
 
-
-        </div>
-    )
+				<div className='flex items-start justify-center relative -top-1'>
+					<Link href='settings'>
+						<SettingsIcon fill='white' width={30} />
+					</Link>
+				</div>
+			</div>
+			{open ? (
+				<div className='absolute transition-all flex items-center justify-center top-0 left-0 w-full h-full bg-black bg-opacity-90 '>
+					<div className='bg-bg-main z-50 border-1 z-40 border-white overflow-hidden w-full h-full '>
+						<Search />
+					</div>
+				</div>
+			) : (
+				<></>
+			)}
+		</div>
+	)
 }
 export default Header
--- src/widgets/shedule/SheduleMainWidget.tsx
+++ src/widgets/shedule/SheduleMainWidget.tsx
@@ -1,42 +1,37 @@
 'use client'
-import React from 'react'
-import {useAppDispatch, useAppSelector} from "@/hooks";
-import SearchIcon from "@/entities/icons/SearchIcon";
-import {searchHandler} from "@/store/slices/searchSlice";
-import SheduleWidget from "@/widgets/shedule/SheduleWidget";
-
+import SearchIcon from '@/entities/icons/SearchIcon'
+import { useAppDispatch, useAppSelector } from '@/hooks'
+import { searchHandler } from '@/store/slices/searchSlice'
+import SheduleWidget from '@/widgets/shedule/SheduleWidget'
 
 const SheduleMainWidget = () => {
-    const dispatch = useAppDispatch()
-    const groups = useAppSelector(state => state.sheduleReducer.initialState.list)
-    const groupId = groups[groups.length - 1]?.groupId
+	const dispatch = useAppDispatch()
+	const groups = useAppSelector(state => state.sheduleReducer.initialState.list)
+	const groupId = groups[groups.length - 1]?.groupId
 
-
-    return (
-        <div>{
-            groupId == "-1" ?
-                <div
-                    className='className="w-full h-[90dvh] font-semibold text-2xl flex items-center justify-center'>
-                    <div className="px-4 ">
-                        <div className="font-normal">Выберете группу</div>
-                        <div
-                            className="flex items-center  w-9/12 justify-between py-3 px-5 bg-bg-header bg-opacity-70 rounded-lg mx-auto my-4 font-normal text-xl ">
-                            <p>Поиск</p>
-                            <p className='flex items-start justify-end w-full'
-                               onClick={() => dispatch(searchHandler())}>
-
-                                <SearchIcon fill={'white'} width={30}/>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                :
-                <div className='h-full'>
-                    <SheduleWidget/>
-                </div>
-
-
-        }</div>
-    )
+	return (
+		<div>
+			{groupId == '-1' ? (
+				<div className='className="w-full h-[90dvh] font-semibold text-2xl flex items-center justify-center'>
+					<div className='px-4 '>
+						<div className='font-normal'>Выберете группу</div>
+						<div className='flex items-center  w-9/12 justify-between py-3 px-5 bg-bg-header bg-opacity-70 rounded-lg mx-auto my-4 font-normal text-xl '>
+							<p>Поиск</p>
+							<p
+								className='flex items-start justify-end w-full'
+								onClick={() => dispatch(searchHandler())}
+							>
+								<SearchIcon fill={'white'} width={30} />
+							</p>
+						</div>
+					</div>
+				</div>
+			) : (
+				<div className='h-full'>
+					<SheduleWidget />
+				</div>
+			)}
+		</div>
+	)
 }
 export default SheduleMainWidget
