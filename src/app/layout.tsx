c9c33a1748653a5da2c48eb4c5ba688627d4ed8d
Daniil Grebnev <danilgrebneff@yandex.ru>
2023-10-17 12:09:50 +0300

    Meta fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,21 +14,12 @@ export default function RootLayout({
 				<link rel='apple-touch-icon' href='/logo192.png' />
 				<link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
 				<link rel='icon' href='/favicon.ico' type='image/x-icon' />
-				<meta property='og:title' content='Расписание' />
-				<meta
-					property='og:site_name'
-					content='Официальное расписание Воронежского Государственного Аграрного Университета'
-				/>
-				<meta property='og:url' content='https://schedule.neuralteam.ru/' />
-				<meta
-					property='og:description'
-					content='Официальное расписание Воронежского Государственного Аграрного Университета. Разработанное студентами для студентов и преподавателей'
-				/>
-				<meta property='og:type' content=' Веб-сайт ' />
-				<meta
-					property='og:image'
-					content='https://sun9-19.userapi.com/impg/0Qm0NMqt1f7sq-Dg-EkBgLKXnFSBlvYdlx7r5w/20aEnT_4XuI.jpg?size=807x571&quality=95&sign=0f2c513e3c0abea62d639d5d0649375c&c_uniq_tag=Kzr1oGpxrXQQ-QtUvohYe0urwxRycpDW7kwBuqhcNp4&type=album'
-				/>
+				<meta property="og:type" content="website">
+<meta property="og:title" content="Расписание ВГАУ">
+<meta property="og:url" content="https://schedule.neuralteam.ru/">
+<meta property="og:image" content="https://sun9-19.userapi.com/impg/0Qm0NMqt1f7sq-Dg-EkBgLKXnFSBlvYdlx7r5w/20aEnT_4XuI.jpg?size=807x571&quality=95&sign=0f2c513e3c0abea62d639d5d0649375c&c_uniq_tag=Kzr1oGpxrXQQ-QtUvohYe0urwxRycpDW7kwBuqhcNp4&type=album">
+<meta property="og:description" content="Официальное расписание Воронежского Государственного Аграрного Университета. Разработанное студентами для студентов и преподавателей"></meta>
+				
 				<meta name='theme-color' content='#212121' />
 				<meta name='msapplication-TileColor' content='#212121' />
 				<meta name='description' content='Расписание ВГАУ' />
